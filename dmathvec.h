a02b0be74d9eba2be1bc982f0b193c1cfce992f7
mudetoday <mudetoday@gmail.com>
2025-12-09 11:07:50 +0300

    const vectors

--- dmathvec.h
+++ dmathvec.h
@@ -9,16 +9,19 @@ typedef struct {
 	int x, y, z;
 } ivec_t;
 
+// Константы | Constants
+
+const ivec_t ivec_zero    = { 0, 0, 0 };
+const ivec_t ivec_right   = { 1, 0, 0 };
+const ivec_t ivec_forward = { 0, 1, 0 };
+const ivec_t ivec_up 	  = { 0, 0, 1 };
+const ivec_t ivec_left 	  = {-1, 0, 0 };
+const ivec_t ivec_back    = { 0,-1, 0 };
+const ivec_t ivec_down    = { 0, 0,-1 };
+
 // Функции | Functions
 
 // Целочисленный вектор | Integer vector
-
-ivec_t ivec_zero()
-{
-	ivec_t vec;
-	vec.x = 0; vec.y = 0; vec.z = 0;
-	return vec;
-}
 
 ivec_t ivec_make(int x, int  y, int z)
 {
@@ -27,7 +30,7 @@ ivec_t ivec_make(int x, int  y, int z)
 	return vec;
 }
 
-ivec_t ivec_add(ivec_t vec1, ivec_t vec2)
+ivec_t ivec_add_ivec(ivec_t vec1, ivec_t vec2)
 {
 	ivec_t vec;
 	vec.x = vec1.x + vec2.x;
@@ -36,12 +39,21 @@ ivec_t ivec_add(ivec_t vec1, ivec_t vec2)
 	return vec;
 }
 
-ivec_t ivec_sub(ivec_t vec1, ivec_t vec2)
+ivec_t ivec_sub_ivec(ivec_t vec1, ivec_t vec2)
 {
 	ivec_t vec;
 	vec.x = vec1.x - vec2.x;
 	vec.y = vec1.y - vec2.y;
 	vec.z = vec1.z - vec2.z;
+	return vec;
+}
+
+ivec_t ivec_mul_ivec(ivec_t vec1, ivec_t vec2)
+{
+	ivec_t vec;
+	vec.x = vec1.x * vec2.x;
+	vec.y = vec1.x * vec2.x;
+	vec.z = vec1.z * vec2.z;
 	return vec;
 }
 
@@ -51,6 +63,5 @@ int ivec_len(ivec_t vec)
 	int len = i_sqrt(a);
 	return len;
 }
-
 
 #endif
